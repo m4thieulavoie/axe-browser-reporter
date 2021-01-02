a35d27c05aa729027d7d7898cf8971cbb0724636
hughockey <h.lemieux66@gmail.com>
2021-01-02 12:04:41 -0500

    requested changes

--- src/Index/Index.ts
+++ src/Index/Index.ts
@@ -14,26 +14,24 @@ const template = html<IndexComponent>`<div class="heading">
     </p>
     <div class="buttons">
       <button class="refresh" @click=${() => triggerAxeCore()}>Refresh</button>
-      <button @click=${(x) => x.collapse()}>Collapse</button>
+      <button @click=${(x) => x.collapse()}>${(x) => x.collapsed ? 'Expand' : 'Collapse'}</button>
       <button class="close" @click=${(x) => x.remove()}>Close</button>
     </div>
   </div>
   <hr />
-  <slot class="${x => x.collapsed ? 'hide':''}"></slot>`;
+  <slot class="${x => x.collapsed}"></slot>`;
 
 @customElement({
   name: "abr-index",
   template,
   styles,
 })
-
 export default class IndexComponent extends FASTElement {
   @attr violationCount = 0;
   @attr({mode: 'boolean'}) collapsed: boolean = JSON.parse(sessionStorage.getItem('collapsed'));
-
+  
   collapse() {
     this.collapsed = !this.collapsed;
     sessionStorage.setItem('collapsed', JSON.stringify(this.collapsed)); 
   }
-  
 }
