c80dacbc74fad99e7a76e9ab677ef93a6de0aca8
Mathieu Lavoie <mathieu.lavoie@logmein.com>
2021-06-27 20:39:09 -0400

    fix build

--- src/Accordion/index.ts
+++
@@ -1,2 +0,0 @@
-require("./Accordion");
-require("./AccordionItem");
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { DesignSystem } from "@microsoft/fast-foundation";
+import { AccordionComponent } from "./Accordion/Accordion";
+import { AccordionItemComponent } from "./Accordion/AccordionItem";
+
 export { setupAxeCore as default } from "./Utils/setup";
 
+DesignSystem.getOrCreate()
+  .withPrefix("abr")
+  .register(AccordionComponent, AccordionItemComponent);
+
 require("./Index/Index");
-require("./Accordion");
 require("./StatusDot/StatusDot");
